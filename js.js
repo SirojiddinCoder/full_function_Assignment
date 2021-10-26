26726a99d3e5ec15413c0ce67ee925a898231a4f
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-10-26 10:27:25 -0700

    funtion math uchun

---
+++ js.js
@@ -0,0 +1,71 @@
+// Function uchun math.pow(); daraja uchun
+
+// function pow (a, b){
+//     return a**b;
+
+// }
+// console.log(pow(2,3));
+// console.log(Math.pow(2,3));
+
+
+// function round(a) {
+//    return (a).toFixed(0);
+
+// }
+// console.log(round(1.5))
+
+// Function for Method floor
+
+// function floor(a) {
+//     return parseInt(a);
+// }
+// console.log(floor(8.9))
+
+// Function for Method floor2
+
+// function floor(a) {
+//   return (a - 0.5).toFixed(0);
+// }
+// console.log(floor(6.001));
+
+
+
+// Function for Method ceil
+
+// function ceil(a) {
+//     return (a + 0.5).toFixed(0);
+// }
+// console.log(ceil(1.001));
+
+
+
+//  funtion * uchun
+
+// function calls (a, b){
+
+//     return a * b;
+// }
+// console.log(calls(2 , 3));
+
+//  funtion ildiz uchun uchun
+
+// function sqrt (a){
+
+//     return a **0.5;
+// }
+// console.log(sqrt(100));
+
+//  funtion  modul  uchun
+
+// function ABS (a,b){
+
+//     return ((a**b)**0.5);
+// }
+// console.log(ABS(-2,2));
+
+  //  funtion    cbrt uchun
+
+// function cbrt(a) {
+//   return (a ** (1/3));
+// }
+//  console.log(cbrt(125));
